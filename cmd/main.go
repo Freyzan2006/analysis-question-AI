690942ac8ef4883414cd0b918d12486a0afc5870
Freyzan2006 <freyzan2006@gmail.com>
2025-08-09 19:43:08 +0500

    Создал app in google, create secret key, write code for read table

--- cmd/main.go
+++ cmd/main.go
@@ -8,33 +8,39 @@ import (
 	"analysis-question-AI/internal/api/http/external"
 )
 
+import (
+	"analysis-question-AI/test_features"
+)
+
 
 func main() {
-	// инициализация переменных окружения
-	var env = core.NewEnvironment()
+	// // инициализация переменных окружения
+	// var env = core.NewEnvironment()
 
-	// инициализация api gemini
-	var API_GEMINI_URL = env.Get("API_GEMINI_URL")
-	var API_GEMINI_KEY = env.Get("API_GEMINI_KEY")
-	var apiGemini = external.NewGeminiAPI(API_GEMINI_URL, API_GEMINI_KEY)
+	// // инициализация api gemini
+	// var API_GEMINI_URL = env.Get("API_GEMINI_URL")
+	// var API_GEMINI_KEY = env.Get("API_GEMINI_KEY")
+	// var apiGemini = external.NewGeminiAPI(API_GEMINI_URL, API_GEMINI_KEY)
 
 
-	// инициализация api google docs
-	var API_GOOGLE_DOCS_URL = env.Get("API_GOOGLE_DOCS_URL")
-	var apiGoogleDocs = external.NewGoogleDocsAPI(API_GOOGLE_DOCS_URL)
+	// // инициализация api google docs
+	// var API_GOOGLE_DOCS_URL = env.Get("API_GOOGLE_DOCS_URL")
+	// var apiGoogleDocs = external.NewGoogleDocsAPI(API_GOOGLE_DOCS_URL)
 
-	// инициализация флагов из командного ряда
-	var flags = cli.NewFlags()
+	// // инициализация флагов из командного ряда
+	// var flags = cli.NewFlags()
 	
-	// Инициализация репозитория
-	var repo = repository.NewQuestionRepository()
+	// // Инициализация репозитория
+	// var repo = repository.NewQuestionRepository()
 
-	// Инициализация сервисов
-	var geminiSvc = service.NewQuestionService(apiGemini, repo)
-	var googleDocsSvc = service.NewGoogleDocsService(apiGoogleDocs, repo)
-	googleDocsSvc.GetQuestions()
+	// // Инициализация сервисов
+	// var geminiSvc = service.NewQuestionService(apiGemini, repo)
+	// var googleDocsSvc = service.NewGoogleDocsService(apiGoogleDocs, repo)
+	// googleDocsSvc.GetQuestions()
 
-	// Инициализация cli команд
-	var commands = cli.NewCommands(flags, geminiSvc, repo, env)
-	commands.Run()
+	// // Инициализация cli команд
+	// var commands = cli.NewCommands(flags, geminiSvc, repo, env)
+	// commands.Run()
+
+	test_features.Test()
 }
